a99e9b9f533fa7aeaab31be4d1cc318c6b0d5ffb
Tarde Cetafaju <Tarde.Cetafaju@labfies.org.br>
2024-01-17 14:48:35 -0300

    test

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { Link, Outlet } from 'react-router-dom'
 import './App.css'
-import Navbar from './components/NavBar'
+import Navbar from './components/Navbar'
 
 function App() {
   return (
